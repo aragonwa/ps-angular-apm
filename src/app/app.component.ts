69be7b6c8207f68e15a7594935b80a6b7afa42f6
Alex Aragon <alexaragon@MacBook-Pro-2.domain>
2018-02-03 10:10:39 -0800

    Adding routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from "@angular/core";
+import { ProductService } from "./products/product.service";
 
 @Component({
   selector: 'pm-root',
   template: `
     <div>
-      <h1>{{pageTitle}}</h1>
-      <pm-products></pm-products>
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <a class="navbar-brand">{{pageTitle}}</a>
+          <ul class="nav navbar-nav">
+            <li><a [routerLink]="['/welcome']">Home</a></li>
+            <li><a [routerLink]="['/products']">Product list</a></li>
+          </ul>
+        </div>
+      </nav>
     </div>
-  `
+    <div class="container">
+      d<router-outlet></router-outlet>
+    </div>
+  `,
+  providers:[ProductService]
 })
 export class AppComponent {
   pageTitle: string = "Acme Product Management"

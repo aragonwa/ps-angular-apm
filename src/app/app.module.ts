06df39f1ed89b1b46b81474eeac0b3b01c25e5d2
Alex Aragon <alexaragon@MacBook-Pro-2.domain>
2018-02-04 08:50:20 -0800

    Creating router modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule} from '@angular/common/http';
 import { WelcomeComponent } from './home/welcome.component';
 import { ProductModule } from './products/product.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -15,14 +14,9 @@ import { ProductModule } from './products/product.module';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
-    RouterModule.forRoot([
-      {path: 'welcome', component: WelcomeComponent},
-      {path: '', redirectTo: 'welcome', pathMatch: 'full'},
-      {path: '**', redirectTo: 'welcome', pathMatch: 'full'},
-    ]),
-    ProductModule
+    ProductModule,
+    AppRoutingModule
   ],
 
   bootstrap: [AppComponent]
